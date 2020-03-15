2c4b5e57a0330edc2c9176a1df727581c05a1488
nibydlo <dp.krylov@gmail.com>
2020-03-15 12:44:35 +0300

    add fit module and train/eval call

--- topics_ds/pytorch/torch_models.py
+++ topics_ds/pytorch/torch_models.py
@@ -8,6 +8,51 @@ TXT_LEN = 300
 N_CLASSES = 50
 
 
+def fit_topics_model(model, optimizer, train_loader, val_loader, scheduler=None, writer=None, epochs=1):
+    for epoch in range(epochs):
+        model.train()
+
+        loss_sum = 0.0
+        loss_count = 0
+
+        for x_img_cur, x_txt_cur, y_cur in train_loader:
+            model.zero_grad()
+            output = model(x_img_cur.view(-1, IMG_LEN).float(), x_txt_cur.view(-1, TXT_LEN).float())
+            loss = F.nll_loss(output, torch.argmax(y_cur, dim=1))
+            loss.backward()
+
+            loss_sum += loss
+            loss_count += 1
+
+            optimizer.step()
+
+        print('epoch:', epoch, 'train_loss:', loss, 'average train loss', loss_sum / loss_count)
+        writer.add_scalar('train_loss', loss, epoch)
+        writer.add_scalar('avg_train_loss', loss_sum / loss_count, epoch)
+
+        model.eval()
+
+        correct = 0
+        total = 0
+        loss_sum = 0.0
+        loss_count = 0
+
+        with torch.no_grad():
+            for x_img_cur, x_txt_cur, y_cur in val_loader:
+                output = model(x_img_cur.view(-1, IMG_LEN).float(), x_txt_cur.view(-1, TXT_LEN).float())
+                loss = F.nll_loss(output, torch.argmax(y_cur, dim=1))
+                loss_sum += loss
+                loss_count += 1
+                for idx, i in enumerate(output):
+                    if torch.argmax(i) == torch.argmax(y_cur, dim=1)[idx]:
+                        correct += 1
+                    total += 1
+
+        print('val_acc:', correct / total, 'val_avg_loss:', loss_sum / loss_count)
+        writer.add_scalar('val_acc', correct / total, epoch)
+        writer.add_scalar('val_avg_loss', loss_sum / loss_count, epoch)
+
+
 class NormModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -21,6 +66,53 @@ class NormModel(nn.Module):
         self.fc1 = nn.Linear(512, 128)
         self.fc2 = nn.Linear(128, 128)
         self.out = nn.Linear(128, N_CLASSES)
+
+        self.dropout = nn.modules.Dropout(p=0.25)
+
+    def forward(self, inp_img, inp_txt):
+        x_img = F.relu(self.fc_img_1(inp_img))
+        x_img = self.dropout(x_img)
+        x_img = F.relu(self.fc_img_2(x_img))
+        x_img = self.dropout(x_img)
+
+        x_txt = F.relu(self.fc_txt_1(inp_txt))
+        x_txt = self.dropout(x_txt)
+        x_txt = F.relu(self.fc_txt_2(x_txt))
+        x_txt = self.dropout(x_txt)
+
+        x = torch.cat((x_img, x_txt), 1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+
+        x = F.log_softmax(self.out(x), dim=1)
+        return x
+
+
+class NormModelGlorot(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.fc_img_1 = nn.Linear(IMG_LEN, 512)
+        nn.init.xavier_uniform_(self.fc_img_1.weight)
+
+        self.fc_img_2 = nn.Linear(512, 256)
+        nn.init.xavier_uniform_(self.fc_img_2.weight)
+
+        self.fc_txt_1 = nn.Linear(TXT_LEN, 256)
+        nn.init.xavier_uniform_(self.fc_txt_1.weight)
+
+        self.fc_txt_2 = nn.Linear(256, 256)
+        nn.init.xavier_uniform_(self.fc_txt_2.weight)
+
+        self.fc1 = nn.Linear(512, 128)
+        nn.init.xavier_uniform_(self.fc1.weight)
+
+        self.fc2 = nn.Linear(128, 128)
+        nn.init.xavier_uniform_(self.fc2.weight)
+
+        self.out = nn.Linear(128, N_CLASSES)
+        nn.init.xavier_uniform_(self.out.weight)
 
         self.dropout = nn.modules.Dropout(p=0.25)
 
@@ -160,8 +252,8 @@ class UAModel1(nn.Module):
     def __init__(self, d=256):
         super().__init__()
 
-        self.fc_img = nn.Linear(IMG_LEN, 128)
-        self.fc_txt = nn.Linear(TXT_LEN, 128)
+        self.fc_img = nn.Linear(IMG_LEN, d // 2)
+        self.fc_txt = nn.Linear(TXT_LEN, d // 2)
 
         self.d = d
 
@@ -179,6 +271,35 @@ class UAModel1(nn.Module):
         return out
 
 
+class UAModel2(nn.Module):
+    def __init__(self, d=32):
+        super().__init__()
+
+        self.fc_img = nn.Linear(IMG_LEN, d // 2)
+        self.fc_txt = nn.Linear(TXT_LEN, d // 2)
+
+        self.d = d
+
+        self.gsa_1 = GSA(d)
+        self.ffn_1 = FFN(d)
+
+        self.gsa_2 = GSA(d)
+        self.ffn_2 = FFN(d)
+
+        self.fc_out = nn.Linear(d, N_CLASSES)
+
+    def forward(self, inp_img, inp_txt):
+        x_img = self.fc_img(inp_img)
+        x_txt = self.fc_txt(inp_txt)
+        z = torch.cat((x_img, x_txt), 1)
+
+        x = self.ffn_1(z, self.gsa_1(z))
+        x = self.ffn_2(x, self.gsa_2(x))
+
+        out = F.log_softmax(self.fc_out(x))
+        return out
+
+
 class TrivialModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -191,4 +312,15 @@ class TrivialModel(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = F.log_softmax(self.fc2(x), dim=1)
+        return x
+
+
+class ExtremeTrivialModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(IMG_LEN + TXT_LEN, N_CLASSES)
+
+    def forward(self, inp_img, inp_txt):
+        x = torch.cat((inp_img, inp_txt), 1)
+        x = F.log_softmax(self.fc(x), dim=1)
         return x
